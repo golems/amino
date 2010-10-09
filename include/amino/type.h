e9d53bf5f2a3ccb3e9b81293b55fe8bbd4a96329
Neil T. Dantam <ntd@gatech.edu>
2010-10-09 18:43:49 +0000

    Add type.h
    git-svn-id: file:///home/svn/humanoids/src/common/lib/amino@4972 4f504ea5-f356-0410-adec-f1b3679f3bb7

---
+++ include/amino/type.h
@@ -0,0 +1,57 @@
+/* -*- mode: C; c-basic-offset: 4  -*- */
+/* ex: set shiftwidth=4 expandtab: */
+/*
+ * Copyright (c) 2010, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the Georgia Tech Research Corporation nor
+ *       the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific
+ *       prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GEORGIA
+ * TECH RESEARCH CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef AMINO_TYPE_H
+#define AMINO_TYPE_H
+
+typedef enum {
+    AA_TYPE_VOID,
+    AA_TYPE_BOOL,
+    AA_TYPE_INT8,
+    AA_TYPE_UINT8,
+    AA_TYPE_INT16,
+    AA_TYPE_UINT16,
+    AA_TYPE_INT32,
+    AA_TYPE_UINT32,
+    AA_TYPE_INT64,
+    AA_TYPE_UINT64,
+    AA_TYPE_FLOAT,
+    AA_TYPE_DOUBLE,
+    AA_TYPE_STRING
+} aa_type_t;
+
+#endif //AMINO_TYPE_H
