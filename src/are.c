ba5be1984d71d07740faf435e5e0650b479b329d
Neil T. Dantam <ntd@gatech.edu>
2012-01-20 19:01:31 -0500

    CARE optimization

--- src/are.c
+++ src/are.c
@@ -54,33 +54,38 @@ static void aa_la_care_laub_hamiltonian( size_t m, size_t n, size_t p,
                                          const double *restrict C,
                                          double *restrict H ) {
     // copy in A
-    for( size_t i = 0; i < m; i ++ ) {
-        for( size_t j = 0; j < m; j ++ ) {
-            // upper left gets A
-            AA_MATREF(H, 2*m, i, j) = AA_MATREF(A, m, i, j);
-            // lower right gets -A^T
-            AA_MATREF(H, 2*m, m+i, m+j) = -AA_MATREF(A, m, j, i);
-        }
-    }
+    // upper left gets A
+    aa_clapack_dlacpy( 0, (int)m, (int)m, A, (int)m, H, 2*(int)m );
+    // lower right gets -A^T
+    aa_la_dtranspose( m, m, A, m,
+                      &AA_MATREF(H, 2*m, m, m), 2*m );
+    aa_clapack_dlascl( 'G', 0, 0,
+                       -1.0, 1.0,
+                       (int)m, (int)m,
+                       &AA_MATREF(H, 2*m, m, m), 2*(int)m );
+
     // copy B
     if( n == m ) {
-        for( size_t i = 0; i < m; i ++ ) {
-            for( size_t j = 0; j < m; j ++ ) {
-                AA_MATREF(H, 2*m, i, m+j) = -AA_MATREF(B, m, i, j);
-            }
-        }
+        aa_clapack_dlacpy( 0, (int)m, (int)m, B, (int)m,
+                           &AA_MATREF(H, 2*m, 0, m), 2*(int)m );
+        aa_clapack_dlascl( 'G', 0, 0,
+                           -1.0, 1.0,
+                           (int)m, (int)m,
+                           &AA_MATREF(H, 2*m, 0, m), 2*(int)m );
     } else {
         cblas_dgemm( CblasColMajor, CblasNoTrans, CblasTrans,
                      (int)m, (int)m, (int)n, -1, B, (int)m, B, (int)m,
                      0, &AA_MATREF(H, 2*m, 0, m), 2*(int)m );
     }
+
     // copy C
     if( p == m ) {
-        for( size_t i = 0; i < m; i ++ ) {
-            for( size_t j = 0; j < m; j ++ ) {
-                AA_MATREF(H, 2*m, i+m, j) = -AA_MATREF(C, m, i, j);
-            }
-        }
+        aa_clapack_dlacpy( 0, (int)m, (int)m, C, (int)m,
+                           &AA_MATREF(H, 2*m, m, 0), 2*(int)m );
+        aa_clapack_dlascl( 'G', 0, 0,
+                           -1.0, 1.0,
+                           (int)m, (int)m,
+                           &AA_MATREF(H, 2*m, m, 0), 2*(int)m );
     } else {
         cblas_dgemm( CblasColMajor, CblasTrans, CblasNoTrans,
                      (int)m, (int)m, (int)p, -1, C, (int)p, C, (int)p,
@@ -103,63 +108,50 @@ AA_API int aa_la_care_laub( size_t m, size_t n, size_t p,
     int mi = (int)m;
     int mi2 = 2*mi;
 
-    double u11t[m*m];
-    double *u21t = X;  // u21 overwritten with X by lapack solver
+    double vs[2*m*2*m];
     {
         // shcur in lapack: dgees, (will balance the array itself)
-        double vs[2*m*2*m];
-        {
-            // build hamiltonian
-            double H[2*m*2*m];
-            aa_la_care_laub_hamiltonian(m,n,p,A,B,C,H);
+        // build hamiltonian
+        double H[2*m*2*m];
+        aa_la_care_laub_hamiltonian(m,n,p,A,B,C,H);
 
-            int info;
-            int lwork = -1;
-            while(1) {
-                int sdim;
-                int bwork[2*m];
-                double wr[2*m];
-                double wi[2*m];
-                double work [ lwork < 0 ? 1 : lwork ];
-                dgees_("V", "S",
-                       aa_la_care_laub_select,
-                       &mi2, H, &mi2, &sdim, wr, wi,
-                       vs, &mi2, work, &lwork, bwork, &info );
-                if( lwork > 0 ) break;
-                // got work array size
-                assert( -1 == lwork && sizeof(work) == sizeof(double) );
-                lwork = (int)work[0];
-            };
-        }
-
-        // compute the resulting thing
-        // vs = scal * vs
-
-        // get u11
-        for( size_t j = 0; j < m; j ++) {
-            for( size_t i = 0; i < m; i ++) {
-                //u11
-                AA_MATREF(u11t, m, j, i) =
-                    AA_MATREF(vs, 2*m, i, j);
-                //u21
-                AA_MATREF(u21t, m, j, i) =
-                    AA_MATREF(vs, 2*m, i+m, j);
-            }
-        }
+        int info;
+        int lwork = -1;
+        while(1) {
+            int sdim;
+            int bwork[2*m];
+            double wr[2*m];
+            double wi[2*m];
+            double work [ lwork < 0 ? 1 : lwork ];
+            dgees_("V", "S",
+                   aa_la_care_laub_select,
+                   &mi2, H, &mi2, &sdim, wr, wi,
+                   vs, &mi2, work, &lwork, bwork, &info );
+            if( lwork > 0 ) break;
+            // got work array size
+            assert( -1 == lwork && sizeof(work) == sizeof(double) );
+            lwork = (int)work[0];
+        };
     }
 
     // solve the least squares problem
     // X * u11 = u21, but X is symmetric, so
-    // u11^T X = u21^T
-    // X = u21 * u11^(-1)
+    // u11^T X^T = u11^T X = u21^T
+
+    // copy u21' to X for dgels
+    aa_la_dtranspose( m, m,
+                      &AA_MATREF(vs, 2*m, m, 0), 2*m, X, m );
     {
         int lwork = -1;
         int info;
         while(1) {
             double work [ lwork < 0 ? 1 : lwork ];
-            dgels_( "N", &mi, &mi, &mi, u11t, &mi, u21t, &mi, work, &lwork,
+            dgels_( "T", &mi, &mi, &mi,
+                    vs, &mi2, /* u11' */
+                    X, &mi,   /* u21' */
+                    work, &lwork,
                     &info );
-            if(lwork > 0 ) break;
+            if(lwork > 0) break;
             assert( -1 == lwork && sizeof(work) == sizeof(double) );
             lwork = (int)work[0];
         };
