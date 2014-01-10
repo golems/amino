f4ce641b9fcc9e40f21848e11caba6594da9921a
Neil T. Dantam <ntd@gatech.edu>
2014-01-09 20:39:34 -0500

    Detect comments in matrix IO

--- include/amino/io.h
+++ include/amino/io.h
@@ -47,8 +47,24 @@
  */
 AA_API ssize_t aa_read_realloc(int fd, void **buf, size_t off, size_t *max);
 
+#define AA_IO_ISCOMMENT(x) ('#' == (x))
 
 /** Read line from file, storing in memory region. */
 char *aa_io_getline( FILE *fin, struct aa_mem_region *reg );
+
+/** Skip leading blank characters */
+char *aa_io_skipblank( const char *str );
+
+/** Parse vector from a line */
+size_t aa_io_parsevector( const char *str, size_t n, double *X, size_t incx, char **endptr );
+
+/** Read a vector from a line in the file */
+size_t aa_io_getvector( FILE *fin, struct aa_mem_region *reg, size_t n, double *X, size_t incx );
+
+/** Read a fixed size matrix from file.
+ *  Returns number of items read.
+ */
+size_t aa_io_fread_matrix_fix( FILE *fin, size_t m, size_t n,
+                               double *A, size_t lda );
 
 #endif //AMINO_IO_H
--- src/io.c
+++ src/io.c
@@ -43,6 +43,7 @@
 
 #include "amino.h"
 #include <unistd.h>
+#include <ctype.h>
 
 AA_API ssize_t aa_read_realloc(int fd, void **buf,
                            size_t off, size_t *max) {
@@ -70,3 +71,66 @@ char *aa_io_getline( FILE *fin, struct aa_mem_region *reg )
     } while(1);
 }
 
+char *aa_io_skipblank( const char *str )
+{
+    const char *ptr = str;
+    while( isblank(*ptr) ) ptr++;
+    return (char*)ptr;
+}
+
+
+size_t aa_io_parsevector( const char *str, size_t n, double *X, size_t incx, char **lendptr )
+{
+    char *ptr = (char*)str; // promise not to change str
+
+    size_t i = 0;
+    for(; i < n; i ++ ) {
+        if( NULL == ptr || AA_IO_ISCOMMENT(*ptr) ) break;
+
+        errno = 0;
+        char *endptr;
+        X[i*incx] = strtod(ptr, &endptr);
+
+        if( ptr == endptr ) break;
+
+        endptr = aa_io_skipblank(endptr);
+
+        ptr = endptr;
+    }
+
+    if( lendptr ) *lendptr = ptr;
+
+    return i;
+}
+
+size_t aa_io_getvector( FILE *fin, struct aa_mem_region *reg, size_t n, double *X, size_t incx )
+{
+    if ( NULL == reg ) reg = aa_mem_region_local_get();
+
+    char *line = aa_io_getline( fin, reg );
+    size_t i = 0;
+    if(line) {
+        i = aa_io_parsevector( line, n, X, incx, NULL );
+        aa_mem_region_pop( reg, line );
+    }
+
+    return i;
+}
+
+
+size_t aa_io_fread_matrix_fix( FILE *fin, size_t m, size_t n,
+                               double *A, size_t lda )
+{
+    struct aa_mem_region *reg = aa_mem_region_local_get();
+    void *ptr = aa_mem_region_tmpalloc(reg,1);
+
+    size_t cnt = 0;
+    for( size_t i = 0; i < m; i ++ ) {
+        cnt += aa_io_getvector( fin, reg,
+                                n, &AA_MATREF(A, lda, i, 0), lda );
+    }
+
+    aa_mem_region_pop( reg, ptr );
+
+    return cnt;
+}
