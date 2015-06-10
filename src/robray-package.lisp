0d5653706b501b237b16453e8eb9345121790c78
Neil T. Dantam <ntd@rice.edu>
2015-06-09 20:25:17 -0500

    Update exports

--- src/robray-package.lisp
+++ src/robray-package.lisp
@@ -1,25 +1,27 @@
 (defpackage :robray
   (:use :cl :alexandria :amino :sycamore :sycamore-util)
   (:export
+
    ;; Frame Types
    :scene-frame-fixed
    :scene-frame-revolute
    :scene-frame-prismatic
+
    ;; Geometry
    :scene-box
    :scene-sphere
    :scene-cylinder
    :scene-cone
+
+
    ;; Draw interface
    :draw-options
    :draw-options-default
    :merge-draw-options
    :get-draw-option
 
-   :draw-geometry
    :draw-tf-axis
 
-   :draw-items
    :item-cylinder-axis
    :item-cone-axis
    :item-frame-axis
@@ -36,8 +38,12 @@
 
    ;; Scene Graph Manipulation
    :scene-graph
-   :scene-graph-add-frame :scene-graph-remove-frame
-   :scene-graph-add-visual
+   :scene-graph-f
+   :scene-graph-remove-frame
    :scene-graph-reparent
    :load-scene-file
+
+   ;; variables
+   :*render-host-alist
+   :*urdf-package-alist
    ))
