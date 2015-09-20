a5fd43faef40f7e5f58adddfb979768b1732392e
Neil T. Dantam <ntd@rice.edu>
2015-09-20 17:20:19 -0500

    Link against FCL

---
+++ include/amino/rx/amino_fcl.h
@@ -0,0 +1,69 @@
+/* -*- mode: C++; c-basic-offset: 4; -*- */
+/* ex: set shiftwidth=4 tabstop=4 expandtab: */
+/*
+ * Copyright (c) 2015, Rice University
+ * All rights reserved.
+ *
+ * Author(s): Neil T. Dantam <ntd@rice.edu>
+ *
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of copyright holder the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef AMINO_RX_FCL_H
+#define AMINO_RX_FCL_H
+
+/* Utility */
+#ifdef __cplusplus
+
+#include <fcl/math/transform.h>
+
+namespace amino {
+namespace fcl {
+
+
+static inline ::fcl::Transform3f
+qutr2fcltf( const double E[7] )
+{
+    const double *q = E + AA_TF_QUTR_Q;
+    const double *v = E + AA_TF_QUTR_V;
+    return ::fcl::Transform3f(::fcl::Quaternion3f( q[AA_TF_QUAT_W],
+                                                   q[AA_TF_QUAT_X],
+                                                   q[AA_TF_QUAT_Y],
+                                                   q[AA_TF_QUAT_Z]),
+                              ::fcl::Vec3f(v[0], v[1], v[2]));
+}
+
+
+} /* namespace fcl */
+} /* namespace amino */
+
+
+#endif /* __cplusplus */
+
+#endif /*AMINO_RX_FCL_H*/
---
+++ src/test/aa_test_fcl.cpp
@@ -0,0 +1,39 @@
+#include <fcl/collision.h>
+#include <fcl/shape/geometric_shapes.h>
+#include "amino.h"
+#include "amino/rx/amino_fcl.h"
+
+
+
+int main( int argc, char **argv)
+{
+    (void) argc; (void) argv;
+
+    double Ea[7] = AA_TF_QUTR_IDENT_INITIALIZER;
+    double Eb[7] = AA_TF_QUTR_IDENT_INITIALIZER;
+    double Ec[7] = {0,0,0,1, 2,0,0};
+
+    boost::shared_ptr<fcl::CollisionGeometry> a( new fcl::Box(1,1,1) );
+
+    fcl::CollisionObject* obj_a = new fcl::CollisionObject(a, amino::fcl::qutr2fcltf(Ea));
+    fcl::CollisionObject* obj_b = new fcl::CollisionObject(a, amino::fcl::qutr2fcltf(Eb));
+    fcl::CollisionObject* obj_c = new fcl::CollisionObject(a, amino::fcl::qutr2fcltf(Ec));
+
+    {
+        fcl::CollisionRequest request;
+        fcl::CollisionResult result;
+        fcl::collide(obj_a, obj_b, request, result);
+        assert( result.isCollision() );
+    }
+
+    {
+        fcl::CollisionRequest request;
+        fcl::CollisionResult result;
+        fcl::collide(obj_a, obj_c, request, result);
+        assert( ! result.isCollision() );
+    }
+
+
+    return 0;
+
+}
