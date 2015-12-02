1158d32c3a5f7acd97959c5d760f9644b3b3ba93
Neil T. Dantam <ntd@rice.edu>
2015-12-02 16:56:03 -0600

    Add lisp build script

---
+++ share/make-aarx.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+COMMON_LISP=$1
+top_srcdir=$2
+
+LD_LIBRARY_PATH=.libs:$LD_LIBRARY_PATH exec $COMMON_LISP --script <<EOF
+(defvar *top-srcdir* "$2")
+(load "$2/share/make-aarx.lisp")
+EOF
