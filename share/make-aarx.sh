82a91eae0959f7c7ae924bc37932ba324bb1472f
Neil T. Dantam <ntd@rice.edu>
2015-12-16 15:20:39 -0600

    Fix LD_LIBRARY_PATH for lisp core build

--- share/make-aarx.sh
+++ share/make-aarx.sh
@@ -3,7 +3,7 @@
 COMMON_LISP=$1
 top_srcdir=$2
 
-LD_LIBRARY_PATH=.libs:$LD_LIBRARY_PATH exec $COMMON_LISP --script <<EOF
+LD_LIBRARY_PATH=../.libs:$LD_LIBRARY_PATH exec $COMMON_LISP --script <<EOF
 (defvar *top-srcdir* "$2")
 (load "$2/share/make-aarx.lisp")
 EOF
