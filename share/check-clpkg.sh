6d72ffd77c94a6c07d170dc60adb62382ec757a2
Neil T. Dantam <ntd@rice.edu>
2016-07-06 21:15:00 -0500

    Include sycamore via git submodule
    * And also in the distribution tarball

--- share/check-clpkg.sh
+++ share/check-clpkg.sh
@@ -6,6 +6,6 @@ cl_pkg=$3
 
 LD_LIBRARY_PATH=.libs:$LD_LIBRARY_PATH exec > /dev/null 2> /dev/null $COMMON_LISP --script <<EOF
 (defvar *top-srcdir* "$2")
-(load "$2/share/load-ql.lisp")
+(load "$2/share/load-quicklisp.lisp")
 (aa-load-system $3)
 EOF
