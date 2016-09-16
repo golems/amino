f0805d9f6fe5a660c7295065f8da57b50f169b60
Neil T. Dantam <ntd@rice.edu>
2016-09-16 01:51:13 -0500

    Use distcheck target for travis-ci

--- install-apt.sh
+++ install-apt.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 sudo apt-get update -qq
-sudo apt-get install build-essential gfortran \
+sudo apt-get install build-essential gfortran f2c \
      autoconf automake libtool autoconf-archive autotools-dev \
      maxima libblas-dev liblapack-dev \
      libglew-dev libsdl2-dev \
      libompl-dev \
+     doxygen groff \
      sbcl \
      default-jdk \
      blender flex povray libav-tools \
