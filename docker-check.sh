124004d6c6b9ec9e3d3b245313ff5f27de282175
Neil T. Dantam <ntd@rice.edu>
2016-09-26 01:52:51 -0500

    Build Baxter demo in Docker tests

--- docker-check.sh
+++ docker-check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for DOCKERFILE in $@; do
-      docker run "amino-$DOCKERFILE" /bin/sh -c "cd /root/amino && autoreconf -i && ./configure && make -k -j $JOBS distcheck";
+      docker run "amino-$DOCKERFILE" /bin/sh -c "cd /root/amino && autoreconf -i && ./configure --enable-demo-baxter && make -k -j $JOBS check && make -k -j $JOBS distcheck";
 done
