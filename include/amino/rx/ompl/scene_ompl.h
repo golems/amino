867f2a384ae87a81cef88568f0522b9c70e84636
Keliang He <keliang.h@gmail.com>
2016-09-30 10:58:04 -0500

    adds header for hooking in new ompl planner

---
+++ include/amino/rx/ompl/scene_ompl.h
@@ -0,0 +1,58 @@
+/* -*- mode: C++; c-basic-offset: 4; -*- */
+/* ex: set shiftwidth=4 tabstop=4 expandtab: */
+/*
+ * Copyright (c) 2015, Rice University
+ * All rights reserved.
+ *
+ * Author(s): Neil T. Dantam <ntd@rice.edu>
+ *
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of copyright holder the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef AMINO_RX_SCENE_OMPL_H
+#define AMINO_RX_SCENE_OMPL_H
+
+#include "amino/rx/ompl/scene_ompl_internal.h"
+
+/**
+ * @file scene_ompl.h
+ * @brief external header for hooking new ompl planner
+ */
+
+AA_API
+void aa_rx_mp_set_planner(struct aa_rx_mp *mp, ompl::base::Planner *p){
+    mp->set_planner(p);
+}
+
+AA_API
+amino::sgSpaceInformation::Ptr aa_rx_mp_get_space_information(const struct aa_rx_mp* mp){
+    return mp->space_information;
+}
+
+#endif /*AMINO_RX_SCENE_OMPL_H*/
