5ccd29be8247a6450a4673679766729fa0297006
Neil T. Dantam <ntd@rice.edu>
2016-10-13 22:59:09 -0500

    Add quicklisp to init file in install scripts

--- script/install/install-quicklisp.sh
+++ script/install/install-quicklisp.sh
@@ -4,6 +4,7 @@ wget -q https://beta.quicklisp.org/quicklisp.lisp
 
 sbcl --load quicklisp.lisp \
      --eval '(quicklisp-quickstart:install)' \
+     --eval '(let ((ql-util::*do-not-prompt* t)) (ql:add-to-init-file))' \
      --eval '(ql:quickload :cffi)' \
      --eval '(ql:quickload :cffi-grovel)' \
      --eval '(ql:quickload :cxml)' \
