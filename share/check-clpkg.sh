a9471f2b95c081218255683e908b28e5549282aa
Neil T. Dantam <ntd@rice.edu>
2017-03-02 14:23:57 -0600

    Fix lisp check

--- share/check-clpkg.sh
+++ share/check-clpkg.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 COMMON_LISP=$1
-top_srcdir=$2
+export top_srcdir=$2
 cl_pkg=$3
+export LD_LIBRARY_PATH=.libs:$LD_LIBRARY_PATH
 
-LD_LIBRARY_PATH=.libs:$LD_LIBRARY_PATH exec > /dev/null 2> /dev/null $COMMON_LISP --script <<EOF
+exec > /dev/null 2> /dev/null $COMMON_LISP --script <<EOF
 (defvar *top-srcdir* "$2")
 (load "$2/share/load-quicklisp.lisp")
 (aa-load-system $3)
