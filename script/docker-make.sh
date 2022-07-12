c476b919d9df1cabad35ae1437b19c67bfc0d6e3
Neil T. Dantam <ndantam@mines.edu>
2022-07-11 23:27:42 -0600

    Update integration tests for travis-ci.com
    * Workaround bug in ompl.pc
    * Change build status button to travis-ci.com, dyalab repo
    * Update test distributions to Debian Bullseye, Ubuntu Focal, and
      Ubuntu Jammy
    * Cleanup .travis.yml
    * Safelist branches main, master, feature/, and bugfix/.

--- script/docker-make.sh
+++ script/docker-make.sh
@@ -1,12 +1,22 @@
 #!/bin/sh -e
 
 if [ -z "$JOBS" ]; then
-    JOBS=8
+    if [ -z `which nproc` ]; then
+        JOBS=8
+    else
+        JOBS=$((2*`nproc`))
+    fi
 fi
 
 for DOCKERFILE in $@; do
     NAME="amino-${DOCKERFILE}-install"
-    docker rm "$NAME" || true
-    docker run --security-opt seccomp=unconfined --name="$NAME" "amino:$DOCKERFILE-dep" /bin/sh -c "cd /root/amino && autoreconf -i && ./configure --enable-demo-baxter && make -k -j $JOBS V=1 distcheck && make install" && ldconfig && \
-        docker commit "$NAME" "amino:${DOCKERFILE}-install"
+    # Remove prior image
+    if [ `docker images $NAME | wc -l` -gt 1 ]; then
+        docker rm "$NAME"
+    fi
+    # Run the tests and commit a new image
+    docker run --security-opt seccomp=unconfined \
+           --name="$NAME" "amino:$DOCKERFILE-dep" \
+           /bin/sh -c "cd /root/amino && autoreconf -i && ./configure --enable-demo-baxter && make -k -j $JOBS V=1 distcheck && make install && ldconfig" \
+        && docker commit "$NAME" "amino:${DOCKERFILE}-install"
 done
