03fef7508fc239241a73ee66933ba8719f310c4a
Neil T. Dantam <ndantam@mines.edu>
2022-07-13 16:12:01 -0600

    Add cicheck make target to run Docker CI test

--- script/docker-make.sh
+++ script/docker-make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 if [ -z "$JOBS" ]; then
-    if [ -z `which nproc` ]; then
+    if [ -z "`which nproc`" ]; then
         JOBS=8
     else
         JOBS=$((2*`nproc`))
@@ -9,14 +9,17 @@ if [ -z "$JOBS" ]; then
 fi
 
 for DOCKERFILE in $@; do
+    DEP="amino:${DOCKERFILE}-dep"
+    IMAGE="amino:${DOCKERFILE}-install"
     NAME="amino-${DOCKERFILE}-install"
     # Remove prior image
-    if [ `docker images $NAME | wc -l` -gt 1 ]; then
+    if [ `docker container ls --all --filter name="^${NAME}\$" | wc -l` -gt 1 ]
+    then
         docker rm "$NAME"
     fi
     # Run the tests and commit a new image
     docker run --security-opt seccomp=unconfined \
-           --name="$NAME" "amino:$DOCKERFILE-dep" \
+           --name="$NAME" "$DEP" \
            /bin/sh -c "cd /root/amino && autoreconf -i && ./configure --enable-demo-baxter && make -k -j $JOBS V=1 distcheck && make install && ldconfig" \
-        && docker commit "$NAME" "amino:${DOCKERFILE}-install"
+        && docker commit "$NAME" "$IMAGE"
 done
